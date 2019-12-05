4fcaafa6b9f782f08043a43f401fee3083a83597
Pierre Bretéché <pbreteche@dawan.fr>
2019-12-05 16:52:39 +0100

    chargement AJAX

--- 08gallerie.js
+++ 08gallerie.js
@@ -1,11 +1,22 @@
 const menuElement = document.querySelector('.menu');
 const viewerImage = document.querySelector('.viewer > img');
 
-const menuLinks = menuElement.querySelectorAll('a');
-for (const link of menuLinks) {
-    link.addEventListener('click', function(event){
-        // this = element attaché à l'écouteur
-        // event.target = element qui a reçu en premier l'événemment
-        viewerImage.src = this.querySelector('img').src;
-    });
-}
+fetch('/images.json').then(function(response){
+    return response.json();
+}).then(function(filenames){
+    let elementsHtml = '';
+    for (const filename of filenames) {
+        elementsHtml = elementsHtml+'<li><a><img src="/images/tux/'+filename+'"></a></li>';
+    }
+    menuElement.innerHTML = elementsHtml;
+    
+    const menuLinks = menuElement.querySelectorAll('a');
+    for (const link of menuLinks) {
+        link.addEventListener('click', function(event){
+            // this = element attaché à l'écouteur
+            // event.target = element qui a reçu en premier l'événemment
+            viewerImage.src = this.querySelector('img').src;
+        });
+    }
+})
+
