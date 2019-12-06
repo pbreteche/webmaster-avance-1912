bb6e25ab766c40acc97c67af97b9de5072ff91a0
Pierre Bretéché <pbreteche@dawan.fr>
2019-12-06 10:51:56 +0100

    local storage

---
+++ 10webstorage.js
@@ -0,0 +1,15 @@
+let clickCount = +window.localStorage.getItem('click-count');
+
+if(clickCount === null) {
+    clickCount = 0;
+}
+
+const clickButton = document.querySelector('#web-storage-button');
+
+clickButton.addEventListener('click', function(){
+    clickCount = clickCount + 1;
+    // clickCount += 1;
+    // clickCount++;
+    // ++clickCount;
+    window.localStorage.setItem('click-count', clickCount);
+});
